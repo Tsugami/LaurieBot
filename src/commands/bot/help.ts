13957456918fbc41153ab25ef7f9a49b4c2476b0
Tsugami <yslan3000@gmail.com>
2020-02-01 20:28:12 -0300

    enviando a mensagem de help no privado

--- src/commands/bot/help.ts
+++ src/commands/bot/help.ts
@@ -1,12 +1,8 @@
-import { Message, Util, MessageReaction, User } from 'discord.js';
+import { Message, Util, MessageReaction, User, DMChannel } from 'discord.js';
 
 import Command, { TFunction } from '@struct/Command';
 import Categories from '@struct/categories';
 import Embed from '@utils/Embed';
-
-interface ArgsI {
-  command: Command | null;
-}
 
 class HelpCommand extends Command {
   constructor() {
@@ -16,41 +12,47 @@ class HelpCommand extends Command {
     });
   }
 
-  async run(msg: Message, t: TFunction, args: ArgsI) {
-    const { command } = args;
-    if (!command) {
-      const categories = Object.values(Categories).filter(x => x.id !== 'bot');
-      const categoriesMainMessage = categories.map(c => `${c.emoji} ${t(`categories:${c.id}.description`)}`).join('\n');
+  async run(msg: Message, t: TFunction) {
+    let dm: DMChannel;
+    try {
+      dm = await msg.author.createDM();
+    } catch (_) {
+      return msg.reply(t('commands:help.failed_message'), {
+        file: { attachment: 'assets/help.gif', name: 'help.gif' },
+      });
+    }
+    if (msg.channel.type !== 'dm') await msg.reply(t('commands:help.warn_message'));
 
-      const embed = new Embed(msg.author);
-      embed.setDescription(`${t('commands:help.message', { author: msg.author })}\n\n${categoriesMainMessage}`);
-      const sent = await msg.reply(embed);
+    const categories = Object.values(Categories).filter(x => x.id !== 'bot');
+    const categoriesMainMessage = categories.map(c => `${c.emoji} ${t(`categories:${c.id}.description`)}`).join('\n');
 
-      if (sent instanceof Message) {
-        categories.forEach(category => {
-          const emoji = Util.parseEmoji(category.emoji);
-          const id = emoji.id ? emoji.id : emoji.name;
-          sent.react(id);
-        });
+    const embed = new Embed(msg.author);
+    embed.setDescription(`${t('commands:help.message', { author: msg.author })}\n\n${categoriesMainMessage}`);
+    const sent = await dm.send(embed);
 
-        let currentCategoryId: string;
-        const filter = (r: MessageReaction, u: User) => u.id === msg.author.id && r.me;
+    if (sent instanceof Message) {
+      categories.forEach(category => {
+        const emoji = Util.parseEmoji(category.emoji);
+        const id = emoji.id ? emoji.id : emoji.name;
+        sent.react(id);
+      });
 
-        const colector = sent.createReactionCollector(filter);
+      let currentCategoryId: string;
+      const filter = (r: MessageReaction, u: User) => u.id === msg.author.id && r.me;
 
-        embed.setDescription('');
-        colector.on('collect', e => {
-          e.remove(msg.author);
-          const category = categories.find(c => c.emoji === e.emoji.toString());
-          if (category && category.id !== currentCategoryId) {
-            currentCategoryId = category.id;
-            embed.fields = [];
-            embed.setTitle(t(`categories:${category.id}.name`));
-            category.forEach(c => embed.addField(`${c.toTitle(t)}`, `${t(`commands:${c.id}.description`)}`));
-            sent.edit(embed);
-          }
-        });
-      }
+      const colector = sent.createReactionCollector(filter);
+
+      embed.setDescription('');
+      colector.on('collect', e => {
+        const category = categories.find(c => c.emoji === e.emoji.toString());
+        if (category && category.id !== currentCategoryId) {
+          currentCategoryId = category.id;
+          embed.fields = [];
+          embed.setTitle(t(`categories:${category.id}.name`));
+          category.forEach(c => embed.addField(`${c.toTitle(t)}`, `${t(`commands:${c.id}.description`)}`));
+          sent.edit(embed);
+        }
+      });
     }
   }
 }
