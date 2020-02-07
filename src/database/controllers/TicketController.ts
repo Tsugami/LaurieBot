41d32f5d1397094fe7dc30a3ba7b9c26b3529d43
Tsugami <yslan3000@gmail.com>
2020-02-07 16:29:45 -0300

    possivel raiz do problema de não atualizar os tickets

--- src/database/controllers/TicketController.ts
+++ src/database/controllers/TicketController.ts
@@ -6,8 +6,6 @@ import Base from './Base';
 class TicketController extends Base<GuildDocument> {
   private ticket = this.data.ticket;
 
-  private tickets = this.data.ticket && this.data.ticket.tickets;
-
   throwTicket() {
     if (!this.data.ticket) {
       throw new Error('MODULO DESATIVADO');
@@ -71,18 +69,18 @@ class TicketController extends Base<GuildDocument> {
   getTicket(user: User, guild: Guild) {
     this.throwTicket();
 
-    if (!this.tickets || !this.tickets.length) return null;
-    return this.tickets.find(
+    if (!this.ticket || !this.ticket.tickets || !this.ticket.tickets.length) return null;
+    return this.ticket.tickets.find(
       ticket => ticket.authorId === user.id && !ticket.closed && guild.channels.has(ticket.channelId),
     );
   }
 
   async openTicket(channel: TextChannel, user: User, category: CategoryTypes) {
     this.throwTicket();
+    if (!this.ticket) return;
+    if (!this.ticket.tickets) this.ticket.tickets = [];
 
-    if (!this.tickets) this.tickets = [];
-
-    this.tickets.push({
+    this.ticket.tickets.push({
       channelId: channel.id,
       authorId: user.id,
       category,
@@ -90,32 +88,36 @@ class TicketController extends Base<GuildDocument> {
 
     await this.save();
 
-    return this.tickets.find(t => t.channelId === channel.id);
+    return this.ticket.tickets.find(t => t.channelId === channel.id);
   }
 
   async closeTicket(query: TextChannel | User) {
     this.throwTicket();
+    if (!this.ticket) return;
 
-    if (this.tickets) {
+    if (this.ticket.tickets) {
       let index = -1;
-      if (query instanceof TextChannel) index = this.tickets.findIndex(t => t.channelId === query.id);
-      if (query instanceof User) index = this.tickets.findIndex(t => t.authorId === query.id && !t.closed);
+      if (query instanceof TextChannel) index = this.ticket.tickets.findIndex(t => t.channelId === query.id);
+      if (query instanceof User) index = this.ticket.tickets.findIndex(t => t.authorId === query.id && !t.closed);
 
       if (index >= 0) {
-        this.tickets[index].closed = true;
+        this.ticket.tickets[index].closed = true;
         await this.save();
-        return this.tickets[index];
+        return this.ticket.tickets[index];
       }
       throw new Error('ticket não existe');
     }
   }
 
   ratingTicket(ticketId: Types.ObjectId, rate: RateTypes) {
-    if (this.tickets) {
+    this.throwTicket();
+    if (!this.ticket) return;
+
+    if (this.ticket.tickets) {
       // eslint-disable-next-line no-underscore-dangle
-      const index = this.tickets.findIndex(tk => tk._id && tk._id.equals(ticketId));
+      const index = this.ticket.tickets.findIndex(tk => tk._id && tk._id.equals(ticketId));
       if (index >= 0) {
-        this.tickets[index].rate = rate;
+        this.ticket.tickets[index].rate = rate;
         return this.save();
       }
     }
