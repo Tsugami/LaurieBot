28937381de6613c1bc652c3763bdb58886f41f48
Tsugami <yslan3000@gmail.com>
2020-03-28 16:25:04 -0300

    fix(pnconfig): implantando ModuleCommand

--- src/commands/configuration/setchannelpn.ts
+++ src/commands/configuration/setchannelpn.ts
@@ -1,58 +1,52 @@
-// import { Message } from 'discord.js';
+import { Message, TextChannel } from 'discord.js';
+import ModuleCommand, { ModuleOptionArgs } from '@struct/ModuleCommand';
 
-// import { guild } from '@database/index';
-// import Command, { Prompt, TFunction } from '@struct/Command';
+const validate = (message: Message, { guildData: { data } }: ModuleOptionArgs) => {
+  return !!(data.penaltyChannel && message.guild.channels.has(data.penaltyChannel));
+};
 
-// type optionTypes = 'off' | 'on';
-// interface ArgsI {
-//   option: optionTypes;
-// }
-
-// const prompt = {
-//   onOption: 'ON',
-//   offOption: 'OFF',
-// };
-
-// class SetChannelPnCommand extends Command {
-//   constructor() {
-//     super('setchannelpn', {
-//       aliases: ['setchannelpn', 'setcanalpn', 'configurarpn'],
-//       userPermissions: 'MANAGE_GUILD',
-//       channelRestriction: 'guild',
-//       category: 'configuration',
-
-//       args: [
-//         {
-//           id: 'option',
-//           type: ['on', 'off'],
-//           prompt: {
-//             start: Prompt('commands:setchannelpn.args.option.start', prompt),
-//             retry: Prompt('commands:setchannelpn.args.option.retry'),
-//           },
-//         },
-//       ],
-//       defaultPrompt: {
-//         cancelWord: 'cancelar',
-//       },
-//     });
-//   }
-
-//   async run(msg: Message, t: TFunction, args: ArgsI) {
-//     const guildData = await guild(msg.guild.id);
-
-//     if (args.option === 'off') {
-//       if (guildData.data.penaltyChannels.includes(msg.channel.id)) {
-//         return msg.reply(t('commands:setchannelpn.off_option.already_disabled'));
-//       }
-//       await guildData.removePenaltyChannel(msg.channel.id);
-//       return msg.reply(t('commands:setchannelpn.off_option.disabled'));
-//     }
-//     if (!guildData.data.penaltyChannels.includes(msg.channel.id)) {
-//       return msg.reply(t('commands:setchannelpn.on_option.already_enabled'));
-//     }
-//     await guildData.addPenaltyChannel(msg.channel.id);
-//     return msg.reply(t('commands:setchannelpn.on_option.enabled'));
-//   }
-// }
-
-// export default SetChannelPnCommand;
+export default new ModuleCommand(
+  'pnconfig',
+  'configurarpn',
+  {
+    aliases: ['setchannelpn', 'setcanalpn', 'configurarpn'],
+    userPermissions: 'MANAGE_GUILD',
+    channelRestriction: 'guild',
+  },
+  [
+    {
+      id: 'enable',
+      aliases: ['ativar'],
+      validate: (m, a) => !validate(m, a),
+      async run(msg, t, args: ModuleOptionArgs & { channel: TextChannel }) {
+        await args.guildData.setPenaltyChannel(args.channel.id);
+        msg.reply(t('commands:pnconfig.enabled', { channel: args.channel.toString() }));
+      },
+    },
+    {
+      id: 'disable',
+      aliases: ['desativar'],
+      validate,
+      async run(msg, t, { guildData }) {
+        await guildData.removePenaltyChannel();
+        msg.reply(t('commands:pnconfig.disabled'));
+      },
+    },
+    {
+      id: 'change_channel',
+      aliases: ['alterar-canal', 'change_channel'],
+      validate,
+      async run(msg, t, { guildData, channel }: ModuleOptionArgs & { channel: TextChannel }) {
+        const oldChannel =
+          msg.guild.channels.get(String(guildData.data.penaltyChannel))?.toString() ||
+          guildData.data.penaltyChannel ||
+          '---';
+        await guildData.setPenaltyChannel(channel.id);
+        msg.reply(t('commands:pnconfig.channel_changed', { oldChannel, channel }));
+      },
+    },
+  ],
+  {
+    channel: ['textChannel', ['enable', 'change_channel']],
+  },
+);
