676a33aee1578dd7c1da6960707a50647e105f02
Tsugami <yslan3000@gmail.com>
2020-04-14 14:57:53 -0300

    fix(ticket): mostrando o comando errado de ativaçao

--- src/commands/ticket/ticket.ts
+++ src/commands/ticket/ticket.ts
@@ -1,26 +1,30 @@
 import Command from '@struct/command/Command';
 import Categories from '@struct/command/categories';
 import LaurieEmbed from '@struct/LaurieEmbed';
+import { guild } from '@database/index';
 
 export default new Command(
   'ticket',
   {
     category: 'configuration',
   },
-  function run(msg, t) {
+  async function run(msg, t) {
+    const guildData = await guild(msg.guild.id);
+
     const prefix = this.getPrefix(msg);
-    return msg.reply(
-      new LaurieEmbed(msg.author)
-        .setAuthor(t('commands:ticket.title'))
-        .addField(t('commands:ticket.what'), t('commands:ticket.what_message'))
-        .addField(t('commands:ticket.how'), t('commands:ticket.how_message'))
-        .addField(
-          t('commons:commands'),
-          Categories.ticket
-            .map(x => `\`${prefix + x.aliases[0]}\` ${t(`commands:${x.id.replace('-', '_')}.description`)}`)
-            .join('\n'),
-        )
-        .addField(t('commands:ticket.warn'), t('commands:ticket.warn_message', { prefix })),
-    );
+    const embed = new LaurieEmbed(msg.author)
+      .setAuthor(t('commands:ticket.title'))
+      .addField(t('commands:ticket.what'), t('commands:ticket.what_message'))
+      .addField(t('commands:ticket.how'), t('commands:ticket.how_message'))
+      .addField(
+        t('commons:commands'),
+        Categories.ticket
+          .map(x => `\`${prefix + x.aliases[0]}\` ${t(`commands:${x.id.replace('-', '_')}.description`)}`)
+          .join('\n'),
+      );
+    if (!guildData.ticket.active) {
+      embed.addField(t('commands:ticket.warn'), t('commands:ticket.warn_message', { command: `${prefix}tkconfig` }));
+    }
+    return msg.reply(embed);
   },
 );
